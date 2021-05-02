3e1d316fccdaf7486a866bce656990fcb46a1c65
NicolasFuentes2015 <nicolas_chl123@hotmail.cl>
2021-05-02 12:01:54 -0400

    manipule el dom y agrege tinymce

--- js/index.js
+++ js/index.js
@@ -1,6 +1,34 @@
 
+tinymce.init({
+    selector: '#descripcion-txt',
+    height: 200,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+  
+
+
 document.querySelector("#registrar-btn").addEventListener("click",()=>{
+    //value es para oibtener el valor unput de texto
     let nombre = document.querySelector("#nombre-txt").value;
-    alert("hola Mundo" + nombre);
+    //esto lo saque de la pagina del tinymce, es para obtener lo escrito
+    let descripcion = tinymce.get("descripcion-txt").getContent();
+    //checed indica si el radiobutton estaseleccionado
+    let legendario = document.querySelector("#legendario-si").checked;
+    //el tipo se obtiene igual que los input
+    let tipo = document.querySelector("#tipo-select").value;
+
+
+    console.log("hola Mundo",nombre,descripcion,legendario,tipo);
 
 });
